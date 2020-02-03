8662cf914c966e0b83fd1dc5b7eea0bc8f08e0e8
okFox <smayfield@gmail.com>
2020-02-02 19:51:49 -0800

    state updated

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -5,20 +5,28 @@ import Stack from '../components/stack/Stack';
 import Experience from '../components/experience/Experience';
 import Projects from '../components/projects/Projects';
 import styles from './Resume.css';
+import { resume } from './resume.json';
 
 class Resume extends Component {
     state = {
-      project: 'team' //or solo for conditional rendering
+      projectSelect: 'team', //or solo for conditional rendering
+      name: resume.header.name,
+      socials: resume.header.socials,
+      intro: resume.intro,
+      stack: resume.stack,
+      projects: resume.projects,
+      experience: resume.experience
     }
     render() {
+      const { projectSelect, name, socials, intro, stack, projects, experience } = this.state;
       return (
         <>
           <div className={styles.resumeWrapper}>
-            <Header className={styles.headerBox} />
-            <Intro className={styles.introBox} />
-            <Stack className={styles.stackBox} />
-            <Projects className={styles.projectsBox} />
-            <Experience className={styles.xpBox} />
+            <Header name={name} socials={socials} />
+            <Intro intro={intro} />
+            <Stack stack={stack} />
+            <Projects projects={projects} projectSelect={projectSelect} />
+            <Experience experience={experience} />
           </div>
         </>
       );
